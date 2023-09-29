dd7a522c80da7b32b403f470b6193ba0922a5e72
Anthonyg5005 <anthonyxboxreal@gmail.com>
2023-09-29 17:00:33 +0000

    improve topic gathering performance

--- WIP Scripts/WikiFinder.py
+++ WIP Scripts/WikiFinder.py
@@ -1,10 +1,12 @@
 import languagemodels as lm
 
-#settings
-lm.set_max_ram('2gb')
+#use smallest model
+lm.set_max_ram('1gb')
 
+#user sends the message
 message = input("User: ")
 
+#extract the topic to find wiki
 topic = lm.chat(f'''
 System: Get one keyword from the user's question and write it. Write only a single word and don't include punctuation like periods.
 User: What is minecraft?
@@ -23,16 +25,22 @@ User: {message}
 Assistant:
 ''')
 
+#switch to higher performing model
+lm.set_max_ram('2gb')
+
+#remove punctuation before getting wiki
 topic = topic.rstrip('.')
 
+#lookup topic on wiki and get a snippet
 lm.store_doc(lm.get_wiki(topic))
-
 wikisnip = lm.get_doc_context(message)
 
+#give snippet as context
 response = lm.chat(f'''
 System: Use this context to respond, "{wikisnip}"
 User: {message}
 Assistant:
 ''')
 
+#print the resulting reply
 print("Bot: " + response)
