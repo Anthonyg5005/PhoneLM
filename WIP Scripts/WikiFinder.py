b2be811d20318d7ffe78ba370443296dea6c9a0a
Anthonyg5005 <anthonyxboxreal@gmail.com>
2023-09-29 19:39:39 +0000

    let user know if anything goes wrong with the search, also fine-tuned prompt a bit better

--- WIP Scripts/WikiFinder.py
+++ WIP Scripts/WikiFinder.py
@@ -1,12 +1,13 @@
+#import the languagemodels module
 import languagemodels as lm
 
-#use smallest model
+#use smallest model -- quicker and seems to work better for this task
 lm.set_max_ram('1gb')
 
 #user sends the message
 message = input("User: ")
 
-#extract the topic to find wiki
+#extract the topic to find wiki by examples
 topic = lm.chat(f'''
 System: Get one keyword from the user's question and write it. Write only a single word and don't include punctuation like periods.
 User: What is minecraft?
@@ -31,16 +32,25 @@ lm.set_max_ram('2gb')
 #remove punctuation before getting wiki
 topic = topic.rstrip('.')
 
-#lookup topic on wiki and get a snippet
+#search for the topic on the wiki
 lm.store_doc(lm.get_wiki(topic))
+
+wikiresult = lm.get_wiki(topic)
+
+#get a relevant snippet of the wiki
 wikisnip = lm.get_doc_context(message)
 
+#if first model failed it's task, show user the failed search, else continue as normal
+if(wikiresult == "No matching wiki page found."):
+    print(f'''System: Couldn't find wiki page. Searched for: "{topic}"''')
+    wikisnip = "No context available"
+    
 #give snippet as context
 response = lm.chat(f'''
-System: Use this context to respond, "{wikisnip}"
+System: Use this context to respond if available, "{wikisnip}"
 User: {message}
 Assistant:
 ''')
 
 #print the resulting reply
-print("Bot: " + response)
+print("Bot: " + response)   
