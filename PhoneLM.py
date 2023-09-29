96fde25340c9e701daf246032ba3a933fc6557ce
Anthony Guijarro <anthonyxboxreal@gmail.com>
2023-09-28 22:46:39 -0500

    Create repo with starting file

---
+++ PhoneLM.py
@@ -0,0 +1,11 @@
+#import the languagemodels module
+import languagemodels as lm
+
+#module settings
+lm.set_max_ram('2gb')
+
+#variables
+message = input("User:")
+
+#print the resulting response
+print("Phone: " + lm.do(message))
