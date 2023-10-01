74b9f18c79ed36b1cdf91f58aa7734f8bbbf8674
Anthony Guijarro <anthonyxboxreal@gmail.com>
2023-10-01 13:35:23 -0500

    update prompt a bit

--- WIP Scripts/plmInstruct.py
+++ WIP Scripts/plmInstruct.py
@@ -20,13 +20,9 @@ message = input("User: ")
 #get the current time and date
 dateaandtime = lm.get_date()
 
-#system message to introduce the chatbot. Taken from https://github.com/oobabooga/text-generation-webui/blob/f2d82f731a3e74c7aa2f284c469170fa60028348/instruction-templates/Guanaco.yaml#L4
+#system message to introduce the chatbot. Base taken from https://github.com/oobabooga/text-generation-webui/blob/f2d82f731a3e74c7aa2f284c469170fa60028348/instruction-templates/Guanaco.yaml#L4
 response = lm.chat(f'''
-        System: A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions. Limited to only 3 interactions. Currently {dateaandtime} {zone}.{daylightsavings} 
-        User: Hello, I am a curious user. I would like to know more about you.
-        Assistant: I am an artificial intelligence assistant. I am here to provide detailed information about various topics. I can answer questions, provide definitions, and give examples. A downside is that I am limited to only a single interaction.
-        User: What is a tree?
-        Assistant: A tree is a woody perennial plant that typically has a single main stem or trunk, which supports branches and leaves above the ground. Trees are an essential part of the plant kingdom and come in various shapes, sizes, and species, with diverse characteristics and adaptations to different environments.
+        System: A chat between a curious user and an intelligence phone assistant. The assistant gives helpful, detailed, and polite answers to the human's questions. Limited to only one interaction, resulting in no memory of any previous interations due to limited resources on a phone. It is currently {dateaandtime} {zone}.{daylightsavings} 
         User: {message}
         Assistant:
 ''')
