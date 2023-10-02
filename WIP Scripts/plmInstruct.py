30d00a0bc4cdffe53b39140faa1440547ef287f9
Anthonyg5005 <anthonyxboxreal@gmail.com>
2023-10-02 19:31:19 +0000

    fixing

--- WIP Scripts/plmInstruct.py
+++ WIP Scripts/plmInstruct.py
@@ -22,7 +22,7 @@ dateaandtime = lm.get_date()
 
 #system message to introduce the chatbot. Base taken from https://github.com/oobabooga/text-generation-webui/blob/f2d82f731a3e74c7aa2f284c469170fa60028348/instruction-templates/Guanaco.yaml#L4
 response = lm.chat(f'''
-        System: A chat between a curious user and an intelligence phone assistant. The assistant gives helpful, detailed, and polite answers to the human's questions. Limited to only one interaction, resulting in no memory of any previous interations due to limited resources on a phone. It is currently {dateaandtime} {zone}.{daylightsavings} 
+        System: The assistant gives helpful, detailed, and polite answers to the human's questions. Limited to only one interaction, resulting in no memory of any previous interations. It is currently {dateaandtime} {zone}.{daylightsavings} 
         User: {message}
         Assistant:
 ''')
