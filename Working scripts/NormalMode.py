fb072345ba736cfbcd5c9b9b4534519b276f70bf
Anthony Guijarro <anthonyxboxreal@gmail.com>
2023-11-02 19:13:36 -0500

    add working scripts

---
+++ Working scripts/NormalMode.py
@@ -0,0 +1,31 @@
+#import the languagemodels and time modules
+import languagemodels as lm
+import time
+
+#use high quality model
+lm.set_max_ram('2gb')
+
+#get the current time zone
+zone = time.tzname[0]
+
+#check for daylight savings
+if(time.localtime().tm_isdst == 1):
+    daylightsavings = "Daylight Savings is in effect."
+else:
+    daylightsavings = "Daylight Savings is not in effect."
+
+#user message to the chatbot
+message = input("User: ")
+
+#get the current time and date
+dateaandtime = lm.get_date()
+
+#system message to introduce the chatbot. Base taken from https://github.com/oobabooga/text-generation-webui/blob/f2d82f731a3e74c7aa2f284c469170fa60028348/instruction-templates/Guanaco.yaml#L4
+response = lm.chat(f'''
+        System: The assistant gives helpful, detailed, and polite answers to the human's questions. Limited to only one interaction, resulting in no memory of any previous interations. It is currently {dateaandtime} {zone}.{daylightsavings} 
+        User: {message}
+        Assistant:
+''')
+
+#print the response
+print("Computer: " + response)
---
+++ Working scripts/UseWikipedia.py
@@ -0,0 +1,59 @@
+#import the languagemodels module
+import languagemodels as lm
+
+#use smallest model -- quicker and seems to work better for this task
+lm.set_max_ram('1gb')
+
+#user sends the message
+message = input("User: ")
+
+#extract the topic to find wiki by examples
+topic = lm.chat(f'''
+System: Get one keyword from the user's question and write it. Write only a single keyword.
+User: Who wrote the play 'Romeo and Juliet'?
+Assistant: William Shakespeare
+User: When was the Declaration of Independence signed?
+Assistant: Declaration of Independence                
+User: What is minecraft?
+Assistant: Minecraft
+User: How many pages are on wikipedia?
+Assistant: Wikipedia
+User: Tell me about netflix.
+Assistant: Netflix
+User: What is the capital of France?
+Assistant: Paris
+User: {message}
+Assistant:
+''')
+
+#switch to higher performing model
+lm.set_max_ram('2gb')
+
+#remove punctuation before getting wiki
+topic = topic.rstrip('.')
+
+#if statement to check if the lm said None, if true, continue without wiki
+if(topic == "None"):
+    print("System: Couldn't find topic in user's question.")
+    wikisnip = "Couldn't get context."
+else:
+    #search for the topic on the wiki
+    lm.store_doc(lm.get_wiki(topic))
+
+    wikiresult = lm.get_wiki(topic)
+
+    #get a relevant snippet of the wiki
+    wikisnip = lm.get_doc_context(message)
+
+    #if first model failed it's task, show user the failed search, else continue as normal
+    if(wikiresult == "No matching wiki page found."):
+        print(f'''System: Couldn't find wiki page, using internal knowledge. Searched for: "{topic}"''')
+        wikisnip = "No context available, respond with internal knowledge"
+    else:
+        print(f'''System: Wiki page found, searched for "{topic}"''')
+
+#give snippet as context
+response = lm.do(f'''Answer using context if relevant: "{wikisnip}" Question: {message}''')
+
+#print the resulting reply
+print("Computer: " + response)
